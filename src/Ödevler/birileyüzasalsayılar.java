99296ccddf7cce76433432f4bad4a50cc9b0a19d
mehmetcancam <111275137+mehmetcancam@users.noreply.github.com>
2022-08-20 21:26:02 +0300

    Add files via upload

---
+++ src/Ödevler/birileyüzasalsayılar.java
@@ -0,0 +1,19 @@
+package Ödevler;
+
+public class birileyüzasalsayılar {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            int sayac = 0;
+            for (int j = i; j >= 1; j--) {
+                if (i % j == 0) {
+                    sayac++;
+
+                }
+            }
+            if (sayac == 2) {
+                System.out.print(i + " ");
+            }
+
+        }
+    }
+}
