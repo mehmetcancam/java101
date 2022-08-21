2a2798db44e2c7958efe58331c93892014d0affc
mehmetcancam <111275137+mehmetcancam@users.noreply.github.com>
2022-08-21 19:08:25 +0300

    Add files via upload

---
+++ src/Ödevler/tersucgen.java
@@ -0,0 +1,23 @@
+package Ödevler;
+import java.util.*;
+public class tersucgen {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("n sayısını giriniz: ");
+        int n = input.nextInt();
+
+
+        for (int i=n-1 ; i >=1; i--) {
+            for (int j = 1; j <= n-i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k<=(2*i-1); k++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+
+
+    }
+}
