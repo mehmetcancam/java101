bc55d427abd6fd4e908e2f4961bf9f2e13a3a7a2
mehmetcancam <111275137+mehmetcancam@users.noreply.github.com>
2022-08-30 16:07:36 +0300

    Add files via upload

---
+++ src/Ödevler/DizidekiHarmonik.java
@@ -0,0 +1,18 @@
+package Ödevler;
+
+public class DizidekiHarmonik {
+    public static void main(String[] args) {
+
+        double [] list= {1,2,3,4,5};
+        double sum =0;
+
+        for (int i =0; i < list.length ; i++){
+            sum += 1/list[i];
+
+        }
+        System.out.println(list.length/sum);
+
+
+
+    }
+}
