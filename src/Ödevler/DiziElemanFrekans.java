1d422e3f7a7fa093b793eb8598eb77e56995dccf
mehmetcancam <111275137+mehmetcancam@users.noreply.github.com>
2022-08-31 17:19:19 +0300

    Add files via upload

---
+++ src/Ödevler/DiziElemanFrekans.java
@@ -0,0 +1,24 @@
+package Ödevler;
+import java.util.Arrays;
+public class DiziElemanFrekans {
+    public static void main(String[] args) {
+        int[] numbers = {10,20,20,10,10,20,5,20};
+        //5 10 10 10 20 20 20 20
+        Arrays.sort(numbers);
+        int n = 1, a = 0;
+
+        for (int i = 0;i < numbers.length; i++){
+            if (numbers[i]!= a){
+                for (int j = i + 1; j < numbers.length; j++){
+                    if (numbers[i] == numbers[j]) {
+                        n++;
+                        a = numbers[i];
+                    }
+                }
+
+                System.out.printf("% d sayisi % d kere tekrar edildi.", numbers[i], n);
+                n = 1;
+            }
+        }
+    }
+}
